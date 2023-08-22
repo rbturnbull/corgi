2047a3bc1e75e7c462e044c6231914100e4ec28e
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2023-08-23 09:25:18 +1000

    :tv: preliminary files for galaxy - more work to come later

---
+++ galaxy/shed_init.sh
@@ -0,0 +1,10 @@
+planemo shed_init \
+    --name=corgi \
+    --owner=rturnbull \
+    --description="Classifier of ORGanelles (Inter alia)" \
+    --remote_repository_url=https://github.com/rbturnbull/corgi \
+    --homepage_url=https://rbturnbull.github.io/corgi/ \
+    --long_description="Classifies DNA sequences using deep learning." \
+    --category="Machine Learning" \
+    --category="Sequence Analysis" \
+    --force
