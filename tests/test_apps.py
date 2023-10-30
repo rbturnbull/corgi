e36c2e3842aa7adbf917c077b21521059d77e26f
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2023-10-30 15:25:02 +1100

    :cop: updating test_apps to work with seqtree

--- tests/test_apps.py
+++ tests/test_apps.py
@@ -1,7 +1,21 @@
 from torchapp.testing import TorchAppTestCase
 from corgi.apps import Corgi
+from pathlib import Path
+from corgi.seqtree import SeqTree
+from polytorch import HierarchicalData
+from unittest.mock import patch
+
+def mock_init(self):
+    self.seqtree = SeqTree.load(Path(__file__).parent/"testdata/seqtree.pkl")
+    self.classification_tree = self.seqtree.classification_tree
+    self.output_types = [
+        HierarchicalData(root=self.classification_tree),
+    ]
+    super(Corgi, self).__init__()
 
 
+
+@patch("corgi.apps.Corgi.__init__", new=mock_init)
 class TestCorgi(TorchAppTestCase):
     app_class = Corgi
 
