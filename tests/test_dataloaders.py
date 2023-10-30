5bf55089faa5da391b0fc93467fa145e799e3263
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2023-10-30 23:54:48 +1100

    :zap: adding functions to create dataloaders

--- tests/test_dataloaders.py
+++ tests/test_dataloaders.py
@@ -31,6 +31,8 @@ def test_hierarchical_dataloader():
     for node in classification_tree.post_order_iter():
         for i in range(node.count):
             seqtree.add(f"{node}-{i}", node, i % partitions)
+
+    # seqtree.save(test_data/"seqtree2.st")
             
     dl = dataloaders.HierarchicalDataloader(seqtree=seqtree, batch_size=4)
 
@@ -56,39 +58,33 @@ def test_hierarchical_dataloader():
         ['rice-1', 'mycobacterium-4', 'novovirus-1', 'arabidopsis-0'],
         ['novovirus-9', 'rice-2', 'salmonella-2', 'mycobacterium-11'],
         ['rotavirus-7', 'arabidopsis-3', 'rotavirus-5', 'arabidopsis-1'],
-        ['mycobacterium-0', 'salmonella-8', 'novovirus-8', 'rice-3'],        
+        ['salmonella-8', 'novovirus-8', 'rice-3', 'rotavirus-6'],    
     ]
     for batch, expected_batch in zip(dl, expected_batches_epoch0):
         assert batch == expected_batch
         
     expected_batches_epoch1 =  [
-        ['rotavirus-6', 'arabidopsis-2', 'mycobacterium-6', 'rice-4'],
-        ['novovirus-2', 'salmonella-4', 'mycobacterium-5', 'rice-0'],
-        ['rotavirus-3', 'novovirus-5', 'salmonella-11', 'arabidopsis-3'],
-        ['rotavirus-4', 'arabidopsis-1', 'salmonella-0', 'mycobacterium-10'],
-        ['novovirus-9', 'rice-4', 'mycobacterium-7', 'novovirus-5'],
-        ['arabidopsis-0', 'rice-3', 'rotavirus-7', 'salmonella-7'],
+        ['arabidopsis-2', 'mycobacterium-6', 'rice-4', 'novovirus-2'],
+        ['salmonella-4', 'mycobacterium-5', 'rice-0', 'rotavirus-3'],
+        ['novovirus-5', 'salmonella-11', 'arabidopsis-3', 'rotavirus-4'],
+        ['arabidopsis-1', 'salmonella-0', 'mycobacterium-10', 'novovirus-9'],
+        ['rice-4', 'mycobacterium-7', 'novovirus-5', 'arabidopsis-0'],
+        ['rice-3', 'rotavirus-7', 'salmonella-7', 'salmonella-3'],
     ]
     for batch, expected_batch in zip(dl, expected_batches_epoch1):
         assert batch == expected_batch
     
 
-def test_hierarchical_dataloader_transforms():
+def test_create_training_dataloader():
     seqtree = SeqTree.load(test_data/"seqtree.pkl")
     seqbank = SeqBank(test_data/"seqbank.sb")
-    dl = dataloaders.HierarchicalDataloader(
-        seqtree=seqtree, 
-        batch_size=4, 
-        after_item=GetXY(seqbank=seqbank, seqtree=seqtree),
-        before_batch=RandomSliceBatch(maximum=4000),
-    )   
-    
+    dl = dataloaders.create_training_dataloader(seqtree=seqtree, seqbank=seqbank, validation_partition=1, batch_size=4, maximum=567)
     total = 0
     for x,y in dl:
         assert x.shape[0] == 4
         assert y.shape[0] == 4
         
-        assert x.shape[1] <= 4000
+        assert x.shape[1] <= 567
         assert type(x) == TensorDNA
         total += x.shape[0]
     
@@ -99,22 +95,43 @@ def test_validation_dataloader():
     seqtree = SeqTree.load(test_data/"seqtree.pkl")
     seqbank = SeqBank(test_data/"seqbank.sb")
     validation_length = 567
+    batch_size = 4
 
-    keys = list(seqtree.keys())
-
-    dl = TfmdDL(
-        dataset=keys,
-        batch_size=4, 
-        after_item=GetXY(seqbank=seqbank, seqtree=seqtree),
-        before_batch=DeterministicSliceBatch(seq_length=validation_length),
-    )   
+    dl = dataloaders.create_validation_dataloader(
+        seqtree=seqtree, 
+        seqbank=seqbank, 
+        batch_size=batch_size, 
+        validation_length=validation_length,
+        validation_partition=1,
+    )
     
     total = 0
     for x,y in dl:
         assert x.shape[0] == y.shape[0]
+        assert x.shape[0] < batch_size
         
         assert x.shape[1] == validation_length
         assert type(x) == TensorDNA
         total += x.shape[0]
 
-    assert total == len(keys)
+    assert total == dl.n == 2
+
+
+def test_dataloaders():
+    validation_length = 567
+    seqtree = SeqTree.load(test_data/"seqtree.pkl")
+    seqbank = SeqBank(test_data/"seqbank.sb")
+    batch_size = 4
+
+    dls = dataloaders.create_dataloaders(seqtree=seqtree, seqbank=seqbank, batch_size=batch_size, validation_partition=0, validation_length=validation_length)
+
+    total = 0
+    for x,y in dls.valid:
+        assert x.shape[0] == y.shape[0]
+        assert x.shape[0] < batch_size
+        
+        assert x.shape[1] == validation_length
+        assert type(x) == TensorDNA
+        total += x.shape[0]
+
+    assert total == dls.valid.n == 1
