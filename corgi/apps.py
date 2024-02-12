460a9d1cbddd5eac715cb2caf726797c096281f8
rturnbull <robert.turnbull@unimelb.edu.au>
2024-02-12 13:16:03 +1100

    :zap: adding output results method for hierarchical predictions

--- corgi/apps.py
+++ corgi/apps.py
@@ -6,15 +6,17 @@ import pandas as pd
 from fastai.data.core import DataLoaders
 from torchapp.util import copy_func, call_func, change_typer_to_defaults, add_kwargs
 from fastai.learner import Learner, load_learner
+import torch
 import torchapp as ta
 from rich.console import Console
 from rich.table import Table
 from rich.box import SIMPLE
-from Bio.SeqIO import FastaIO
+# from Bio.SeqIO import FastaIO
 import numpy as np
 from polytorch import PolyLoss, HierarchicalData, total_size
 from polytorch.metrics import HierarchicalGreedyAccuracy
 from seqbank import SeqBank
+from hierarchicalsoftmax.inference import node_probabilities, greedy_predictions, render_probabilities
 
 from . import dataloaders, models, refseq, transforms
 from .seqtree import SeqTree
@@ -244,99 +246,250 @@ class Corgi(ta.TorchApp):
         min_length:int = 128,
         **kwargs,
     ):
-        self.seqio_dataloader = dataloaders.SeqIODataloader(files=file, device=learner.dls.device, batch_size=batch_size, max_length=max_length, max_seqs=max_seqs, min_length=min_length)
-        self.categories = learner.dls.vocab
-        return self.seqio_dataloader
+        self.dataloader = dataloaders.SeqIODataloader(files=file, device=learner.dls.device, batch_size=batch_size, max_length=max_length, max_seqs=max_seqs, min_length=min_length)
+        # self.classification_tree = learner.dls.classification_tree # TODO: Add back in
+        return self.dataloader
+
+    # def output_results(
+    #     self,
+    #     results,
+    #     output_dir:Path = ta.Param(default=None, help="A path to output the results as a CSV."),
+    #     csv: Path = ta.Param(default=None, help="A path to output the results as a CSV. If not given then a default name is chosen inside the output directory."),
+    #     save_filtered:bool = ta.Param(default=True, help="Whether or not to save the filtered sequences."),
+    #     threshold: float = ta.Param(
+    #         default=None, 
+    #         help="The threshold to use for filtering. "
+    #             "If not given, then only the most likely category used for filtering.",
+    #     ),
+    #     **kwargs,
+    # ):
+    #     if not output_dir:
+    #         time_string = time.strftime("%Y_%m_%d-%I_%M_%S_%p")
+    #         output_dir = f"corgi-output-{time_string}"
+
+    #     output_dir = Path(output_dir)
+
+    #     chunk_details = pd.DataFrame(self.seqio_dataloader.chunk_details, columns=["file", "accession", "chunk"])
+    #     predictions_df = pd.DataFrame(results[0].numpy(), columns=self.categories)
+    #     results_df = pd.concat(
+    #         [chunk_details.drop(columns=['chunk']), predictions_df],
+    #         axis=1,
+    #     )
+
+    #     # Average over chunks
+    #     results_df = results_df.groupby(["file", "accession"]).mean().reset_index()
+
+    #     columns = set(predictions_df.columns)
+
+    #     results_df['prediction'] = results_df[self.categories].idxmax(axis=1)
+    #     results_df['eukaryotic'] = predictions_df[list(columns & set(refseq.EUKARYOTIC))].sum(axis=1)
+    #     results_df['prokaryotic'] = predictions_df[list(columns & set(refseq.PROKARYOTIC))].sum(axis=1)
+    #     results_df['organellar'] = predictions_df[list(columns & set(refseq.ORGANELLAR))].sum(axis=1)
+
+    #     if not csv:
+    #         output_dir.mkdir(parents=True, exist_ok=True)
+    #         csv = output_dir / f"corgi-output.csv"
+
+    #     console.print(f"Writing results for {len(results_df)} sequences to: {csv}")
+    #     results_df.to_csv(csv, index=False)
+
+    #     # Write all the sequences to fasta files
+    #     if save_filtered:
+    #         record_to_string = FastaIO.as_fasta
+
+    #         output_dir.mkdir(parents=True, exist_ok=True)
+            
+    #         file_handles = {}
+
+    #         for file, record in self.seqio_dataloader.iter_records():
+    #             row = results_df[ (results_df.accession == record.id) & (results_df.file == file) ]
+    #             if len(row) == 0:
+    #                 categories = ["unclassified"]
+    #             else:
+    #                 # Get the categories to write to
+    #                 if not threshold:
+    #                     # if no threshold then just use the most likely category
+    #                     categories = [row['prediction'].item()]
+    #                 else:
+    #                     # otherwise use all categories above or equal to the threshold
+    #                     category_predictions = row.iloc[0][self.categories]
+    #                     categories = [category_predictions[category_predictions >= threshold].index.item()]
+
+    #             for category in categories:
+    #                 if category not in file_handles:
+    #                     file_path = output_dir / f"{category}.fasta"
+    #                     file_handles[category] = open(file_path, "w")
+
+    #                 file_handle = file_handles[category]
+    #                 file_handle.write(record_to_string(record))
+
+    #         for file_handle in file_handles.values():
+    #             file_handle.close()
+
+    #     # Output bar chart
+    #     from termgraph.module import Data, BarChart, Args
+
+    #     value_counts = results_df['prediction'].value_counts()
+    #     data = Data([[count] for count in value_counts], value_counts.index)
+    #     chart = BarChart(
+    #         data,
+    #         Args(
+    #             space_between=False,
+    #         ),
+    #     )
+
+    #     chart.draw()
 
     def output_results(
         self,
         results,
-        output_dir:Path = ta.Param(default=None, help="A path to output the results as a CSV."),
-        csv: Path = ta.Param(default=None, help="A path to output the results as a CSV. If not given then a default name is chosen inside the output directory."),
-        save_filtered:bool = ta.Param(default=True, help="Whether or not to save the filtered sequences."),
-        threshold: float = ta.Param(
-            default=None, 
-            help="The threshold to use for filtering. "
-                "If not given, then only the most likely category used for filtering.",
-        ),
+        seqtree:Path=None, # HACK
+        output_csv: Path = ta.Param(default=None, help="A path to output the results as a CSV."),
+        output_tips_csv: Path = ta.Param(default=None, help="A path to output the results as a CSV which only stores the probabilities at the tips."),
+        output_fasta: Path = ta.Param(default=None, help="A path to output the results in FASTA format."),
+        image_dir: Path = ta.Param(default=None, help="A directory to output the results as images."),
+        image_format:str = "svg",
+        image_threshold:float = 0.005,
+        prediction_threshold:float = ta.Param(default=0.5, help="The threshold value for making hierarchical predictions."),
         **kwargs,
     ):
-        if not output_dir:
-            time_string = time.strftime("%Y_%m_%d-%I_%M_%S_%p")
-            output_dir = f"corgi-output-{time_string}"
+        
+        # Hack - this should be saved from the learner
+        seqtree = SeqTree.load(seqtree)
+        self.classification_tree = seqtree.classification_tree
 
-        output_dir = Path(output_dir)
+        assert self.classification_tree # This should be saved from the learner
+        
+        def node_lineage_string(node) -> str:
+            return "/".join([str(n) for n in node.ancestors[1:]] + [str(node)])
 
-        chunk_details = pd.DataFrame(self.seqio_dataloader.chunk_details, columns=["file", "accession", "chunk"])
-        predictions_df = pd.DataFrame(results[0].numpy(), columns=self.categories)
+        classification_probabilities = node_probabilities(results[0], root=self.classification_tree)
+        category_names = [node_lineage_string(node) for node in self.classification_tree.node_list if not node.is_root]
+        chunk_details = pd.DataFrame(self.dataloader.chunk_details, columns=["file", "original_id", "chunk"])
+        predictions_df = pd.DataFrame(classification_probabilities.numpy(), columns=category_names)
+
         results_df = pd.concat(
             [chunk_details.drop(columns=['chunk']), predictions_df],
             axis=1,
         )
 
         # Average over chunks
-        results_df = results_df.groupby(["file", "accession"]).mean().reset_index()
+        results_df["chunk_index"] = results_df.index
+        results_df = results_df.groupby(["file", "original_id"]).mean().reset_index()
 
-        columns = set(predictions_df.columns)
+        # sort to get original order
+        results_df = results_df.sort_values(by="chunk_index").drop(columns=["chunk_index"])
+        
+        # Get new tensors now that we've averaged over chunks
+        classification_probabilities = torch.as_tensor(results_df[category_names].to_numpy()) 
 
-        results_df['prediction'] = results_df[self.categories].idxmax(axis=1)
-        results_df['eukaryotic'] = predictions_df[list(columns & set(refseq.EUKARYOTIC))].sum(axis=1)
-        results_df['prokaryotic'] = predictions_df[list(columns & set(refseq.PROKARYOTIC))].sum(axis=1)
-        results_df['organellar'] = predictions_df[list(columns & set(refseq.ORGANELLAR))].sum(axis=1)
-
-        if not csv:
-            output_dir.mkdir(parents=True, exist_ok=True)
-            csv = output_dir / f"corgi-output.csv"
-
-        console.print(f"Writing results for {len(results_df)} sequences to: {csv}")
-        results_df.to_csv(csv, index=False)
-
-        # Write all the sequences to fasta files
-        if save_filtered:
-            record_to_string = FastaIO.as_fasta
-
-            output_dir.mkdir(parents=True, exist_ok=True)
-            
-            file_handles = {}
-
-            for file, record in self.seqio_dataloader.iter_records():
-                row = results_df[ (results_df.accession == record.id) & (results_df.file == file) ]
-                if len(row) == 0:
-                    categories = ["unclassified"]
-                else:
-                    # Get the categories to write to
-                    if not threshold:
-                        # if no threshold then just use the most likely category
-                        categories = [row['prediction'].item()]
-                    else:
-                        # otherwise use all categories above or equal to the threshold
-                        category_predictions = row.iloc[0][self.categories]
-                        categories = [category_predictions[category_predictions >= threshold].index.item()]
-
-                for category in categories:
-                    if category not in file_handles:
-                        file_path = output_dir / f"{category}.fasta"
-                        file_handles[category] = open(file_path, "w")
-
-                    file_handle = file_handles[category]
-                    file_handle.write(record_to_string(record))
-
-            for file_handle in file_handles.values():
-                file_handle.close()
-
-        # Output bar chart
-        from termgraph.module import Data, BarChart, Args
-
-        value_counts = results_df['prediction'].value_counts()
-        data = Data([[count] for count in value_counts], value_counts.index)
-        chart = BarChart(
-            data,
-            Args(
-                space_between=False,
-            ),
+        # get greedy predictions which can use the raw activation or the softmax probabilities
+        predictions = greedy_predictions(
+            classification_probabilities, 
+            root=self.classification_tree, 
+            threshold=prediction_threshold,
         )
 
-        chart.draw()
+        results_df['greedy_prediction'] = [
+            node_lineage_string(node)
+            for node in predictions
+        ]
+
+        results_df['accession'] = results_df['original_id'].apply(lambda x: x.split("#")[0])
+        def get_original_classification(original_id:str):
+            if "#" in original_id:
+                return original_id.split("#")[1]
+            return "null"
+        
+        def get_prediction_probability(row):
+            prediction = row["greedy_prediction"]
+            if prediction in row:
+                return row[prediction]
+            return 1.0
+        
+        results_df['probability'] = results_df.apply(get_prediction_probability, axis=1)
+        results_df['original_classification'] = results_df['original_id'].apply(get_original_classification)
+
+        # Reorder columns
+        results_df = results_df[["file", "accession", "greedy_prediction", "probability", "original_id", "original_classification" ] + category_names]
+
+        # Output images
+        if image_dir:
+            console.print(f"Writing inference probability renders to: {image_dir}")
+            image_dir = Path(image_dir)
+            image_paths = []
+            for _, row in results_df.iterrows():
+                filepath = row['file']
+                accession = row['accession']
+                image_path = image_dir / Path(filepath).name / f"{accession}.{image_format}"
+                image_paths.append(image_path)
+            render_probabilities(
+                root=self.classification_tree, 
+                filepaths=image_paths,
+                probabilities=classification_probabilities,
+                predictions=predictions,
+                threshold=image_threshold,
+            )
+
+        if not (image_dir or output_fasta or output_csv or output_tips_csv):
+            print("No output files requested.")
+
+        if output_fasta:
+            raise NotImplementedError
+        #     console.print(f"Writing results for {len(results_df)} repeats to: {output_fasta}")
+        #     with open(output_fasta, "w") as fasta_out:
+        #         for file in self.dataloader.files:
+        #             for record in SeqIO.parse(file, "fasta"):
+        #                 original_id = record.id
+        #                 row = results_df.loc[results_df.original_id == original_id]
+        #                 if len(row) == 0:
+        #                     SeqIO.write(record, fasta_out, "fasta")
+        #                     continue
+
+        #                 accession = row['accession'].item()
+        #                 original_classification = row["original_classification"].item()
+        #                 prediction = row["greedy_prediction"].item()
+                        
+        #                 new_id = f"{accession}#{prediction}"
+        #                 record.id = new_id
+                        
+        #                 # Adapt description
+        #                 record.description = record.description.replace(original_id, "")
+        #                 last_bracket = record.description.rfind(")")
+        #                 if last_bracket == -1:
+        #                     record.description = f"{record.description} ( "
+        #                 else:
+        #                     record.description = record.description[:last_bracket].rstrip() + ", "
+
+        #                 if prediction in row:
+        #                     new_probability = row[prediction].values[0]
+        #                 else:
+        #                     new_probability = 1.0 # i.e.root
+        #                 record.description = f"{record.description} original classification = {original_classification}, classification probability = {new_probability:.2f} )"
+
+        #                 SeqIO.write(record, fasta_out, "fasta")
+
+        if output_tips_csv:
+            output_tips_csv = Path(output_tips_csv)
+            output_tips_csv.parent.mkdir(exist_ok=True, parents=True)
+            non_tips = [node_lineage_string(node) for node in self.classification_tree.node_list if not node.is_leaf]
+            tips_df = results_df.drop(columns=non_tips)
+            tips_df.to_csv(output_tips_csv, index=False)
+
+        if output_csv:
+            output_csv = Path(output_csv)
+            output_csv.parent.mkdir(exist_ok=True, parents=True)
+            console.print(f"Writing results for {len(results_df)} repeats to: {output_csv}")
+            results_df.to_csv(output_csv, index=False)
+
+        # if self.vector:
+        #     # x = results_df.to_xarray()
+        #     # breakpoint()
+        #     # embeddings = xr.DataArray(results[0][1], dims=("accession", "embedding"))
+        #     # embeddings.to_netcdf("embeddings.nc")
+        #     torch.save(results[0][1], "embeddings.pkl")
+
+        return results_df
 
     def category_counts_dataloader(self, dataloader, description):
         from collections import Counter
